823ee62e831340e549d9976d3c823df89c53aab4
wesselt <wesselt@aule.net>
2019-11-26 21:33:18 +0000

    Moved to bootstrap frontend

--- src/store/getters.js
+++
@@ -1,3 +0,0 @@
-export const getConnectionStatus = state => state.connected
-export const getCode = state => state.code
-export const getGuid = state => state.guid
--- src/store/mutation-types.js
+++
@@ -1,3 +0,0 @@
-export const AUTH_REQUEST = 'AUTH_REQUEST'
-export const IS_CONNECTED = 'IS_CONNECTED'
-export const LOGOUT = 'LOGOUT'
