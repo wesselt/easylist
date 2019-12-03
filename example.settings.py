738e77608ccfb11eddab12fe32ea97b135c9b51d
wesselt <wesselt@aule.net>
2019-12-03 08:26:44 +0100

    New settings in example

--- example.settings.py
+++ example.settings.py
@@ -8,4 +8,17 @@ def get_db_password():
    return "your db password here"
 
 def get_url():
-   return "your url here"
+    return "https://easylist.aule.net/"
+    #return "https://easylist.aule.net/sandbox/"
+
+def get_bunq_url():
+    return "https://api.bunq.com/"
+    #return "https://public-api.sandbox.bunq.com/"
+
+def get_oauth_url():
+    return "https://oauth.bunq.com/"
+    #return "https://oauth.sandbox.bunq.com/"
+
+def get_token_url():
+    return "https://api.oauth.bunq.com/"
+    #return "https://api-oauth.sandbox.bunq.com/"
