66236430975d4338f311fef714596e7f2d3047b9
wesselt <wesselt@aule.net>
2020-08-09 15:13:59 +0200

    Retry button

--- sheet.py
+++ sheet.py
@@ -1,6 +1,5 @@
-from urllib.parse import parse_qs
+import urllib.parse
 import json
-import os
 from pprint import pprint
 import requests
 
@@ -15,107 +14,140 @@ import guidhelper
 import settings
 
 
-def error(message):
-    return [json.dumps({"error": message}).encode()]
+class Sheet:
+
+    def __init__(self, env, start_response):
+        self.env = env
+        self.start_response = start_response
+        self.row = None
 
 
-def get_flow():
-    flow = google_auth_oauthlib.flow.Flow.from_client_config(
-        settings.get_app_credentials(), 
-        ['https://www.googleapis.com/auth/drive.file'])
-    flow.redirect_uri = 'https://easylist.aule.net/dev/sheet'
-    return flow
+    def get_url(self):
+        return "{}://{}{}".format(self.env["REQUEST_SCHEME"],
+                                  self.env["SERVER_NAME"],
+                                  self.env["REQUEST_URI"])
+
+    def error(self, message):
+        if self.row and self.row.get("bearer"):
+            retry = (settings.get_url() + "sheet?state=" + self.guid +
+                "&createsheet=1")
+        else:
+            retry = settings.get_url() + "sheet"
+
+        if not isinstance(message, str):
+            message = json.dumps(message)
+        error_url = "error.html?error={}&retry={}".format(
+            urllib.parse.quote(message),
+            urllib.parse.quote(retry))
+        self.start_response('302 Found', [('Location', error_url)])
+        return []
 
 
-# -----------------------------------------------------------------------------
-
-def request_bunq_oauth(env, start_response):
-    url = (settings.get_oauth_url() + 'auth?' +
-           'response_type=code&' +
-           'client_id=' + settings.get_client_id() + '&' +
-           'redirect_uri=' + settings.get_url() + 'sheet&' +
-           'state=' + guidhelper.new_guid())
-    start_response('302 Found', [('Location', url)])
-    return []
+    def get_flow(self):
+        flow = google_auth_oauthlib.flow.Flow.from_client_config(
+            settings.get_app_credentials(), 
+            ['https://www.googleapis.com/auth/drive.file'])
+        flow.redirect_uri = settings.get_url() + "sheet"
+        return flow
 
 
-def create_bunq_installation(env, start_response, guid, code):
-    row = db.new_row(guid)
-    url = settings.get_token_url() + "v1/token"
-    response = requests.post(url, params={
-        "grant_type": "authorization_code",
-        "code": code,
-        "redirect_uri": settings.get_sheet_url(),
-        "client_id": settings.get_client_id(),
-        "client_secret": settings.get_client_secret()
-    }).json()
-    if "error" in response:
-        start_response('200 OK', [('Content-Type','text/json')])
-        return [json.dumps(response).encode()]
-    if "access_token" not in response:
-        start_response('200 OK', [('Content-Type','text/json')])
-        return [json.dumps({"error": "No access_token returned", 
-                            "raw_reply": response}).encode()]
-    row["bearer"] = response["access_token"]
-    db.put_row(row)
-    flow = get_flow()
-    authorization_url, state = flow.authorization_url(
-        access_type='offline',
-        prompt='consent',
-        state=guid,
-        include_granted_scopes='true')
-    start_response('302 Found', [('Location', authorization_url)])
-    return []
+    # -----------------------------------------------------------------------------
+
+    def request_bunq_oauth(self):
+        url = (settings.get_oauth_url() + 'auth?' +
+               'response_type=code&' +
+               'client_id=' + settings.get_client_id() + '&' +
+               'redirect_uri=' + settings.get_url() + 'sheet&' +
+               'state=' + guidhelper.new_guid())
+        self.start_response('302 Found', [('Location', url)])
+        return []
 
 
-def create_sheet(env, start_response, guid, code):
-    url = "{}://{}{}".format(env["REQUEST_SCHEME"],
-                             env["SERVER_NAME"],
-                             env["REQUEST_URI"])
-    flow = get_flow()
-    flow.fetch_token(authorization_response=url)
-    credentials = flow.credentials
-    service = discovery.build('sheets', 'v4', credentials=credentials)
-    spreadsheet = {
-        'properties': {
-            'title': "Easylist"
+    def create_bunq_installation(self, code):
+        url = settings.get_token_url() + "v1/token"
+        response = requests.post(url, params={
+            "grant_type": "authorization_code",
+            "code": code,
+            "redirect_uri": settings.get_url() + "sheet",
+            "client_id": settings.get_client_id(),
+            "client_secret": settings.get_client_secret()
+        }).json()
+        if "error" in response:
+            return self.error(response)
+        if "access_token" not in response:
+            return self.error({
+                "error": "No access_token returned", 
+                "raw_reply": response
+            })
+        self.row = db.new_row(self.guid)
+        self.row["bearer"] = response["access_token"]
+        db.put_row(self.row)
+        return self.request_google_oauth()
+
+
+    def request_google_oauth(self):
+        flow = self.get_flow()
+        authorization_url, state = flow.authorization_url(
+            access_type='offline',
+            prompt='consent',
+            state=self.guid,
+            include_granted_scopes='true')
+        self.start_response('302 Found', [('Location', authorization_url)])
+        return []
+
+
+    def create_sheet(self, code):
+        url = self.get_url()
+        flow = self.get_flow()
+        flow.fetch_token(authorization_response=url)
+        credentials = flow.credentials
+        service = discovery.build('sheets', 'v4', credentials=credentials)
+        spreadsheet = {
+            'properties': {
+                'title': "Easylist"
+            }
         }
-    }
-    spreadsheet = service.spreadsheets().create(body=spreadsheet,
+        spreadsheet = service.spreadsheets().create(body=spreadsheet,
                                fields='spreadsheetId,spreadsheetUrl').execute()
-    spreadsheet_id = spreadsheet.get("spreadsheetId")
-    csv_url = settings.get_url() + "generate?guid=" + guid
-    body = {
-        "values": [
-            ['=importdata("' + csv_url + '")']
-        ]
-    }
-    request = service.spreadsheets().values().update(
-        spreadsheetId=spreadsheet_id,
-        range="A1",
-        valueInputOption="USER_ENTERED",
-        body=body)
-    response = request.execute()
-    sheet_url = spreadsheet.get("spreadsheetUrl")
-    start_response('302 Found', [('Location', sheet_url)])
-    return []
+        spreadsheet_id = spreadsheet.get("spreadsheetId")
+        csv_url = settings.get_url() + "generate?guid=" + self.guid
+        body = {
+            "values": [
+                ['=importdata("' + csv_url + '")']
+            ]
+        }
+        request = service.spreadsheets().values().update(
+            spreadsheetId=spreadsheet_id,
+            range="A1",
+            valueInputOption="USER_ENTERED",
+            body=body)
+        response = request.execute()
+        sheet_url = spreadsheet.get("spreadsheetUrl")
+        self.start_response('302 Found', [('Location', sheet_url)])
+        return []
+
+    def run(self):
+        d = urllib.parse.parse_qs(self.env["QUERY_STRING"])
+        if not "state" in d:
+            return self.request_bunq_oauth()
+        self.guid = d["state"][0]
+        if guidhelper.validate_uuid4(self.guid):
+            return self.error("State must be a valid guid")
+        self.row = db.get_row(self.guid)
+        if "error" in d:
+            return self.error(d["error"][0])
+        if "createsheet" in d:
+            if not self.row or not self.row.get("bearer"):
+                return self.error("BUNQ token required to create sheet")
+            return self.request_google_oauth()
+        if "code" not in d:
+            return error("Parameter code expected")
+        code = d["code"][0]
+        if not self.row:
+            return self.create_bunq_installation(code)
+        return self.create_sheet(code)
 
 
 def application(env, start_response):
-    d = parse_qs(env["QUERY_STRING"])
-    if "error" in d:
-        start_response('200 OK', [('Content-Type','text/json')])
-        return error(d["error"][0])
-    if not "state" in d:
-        return request_bunq_oauth(env, start_response)
-    state = d["state"][0]
-    if guidhelper.validate_uuid4(state):
-        return error("State must be a valid guid")
-    if "code" not in d:
-        start_response('200 OK', [('Content-Type','text/json')])
-        return error("Parameter code expected")
-    code = d["code"][0]
-    row = db.get_row(state)
-    if not row:
-        return create_bunq_installation(env, start_response, state, code)
-    return create_sheet(env, start_response, state, code)
+    sheet = Sheet(env, start_response)
+    return sheet.run()
